1ea3f7e9ab0d45162615f7a905f3bbf9a48943db
TJ Helm <thelm@nait.ca>
2023-10-29 19:56:03 -0600

    Removed HeartEngine repo instance

--- HeartEngine/tools/Main.cmake
+++
@@ -1,8 +0,0 @@
-function(LoadModule target modulePath includeSubPath moduleName)
-    add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/${modulePath})
-    target_include_directories(${target} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/${modulePath}/${includeSubPath})
-    target_link_libraries(${target} PUBLIC ${moduleName})
-endfunction()
-
-# Heart Engine
-# LoadModule(${PROJECT_NAME} core include HeartEngine)
